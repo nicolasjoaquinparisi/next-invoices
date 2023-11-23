f4ab6199eca4e7084cb92cd0eac9ad725a36e42e
Nicolás Joaquín Parisi <78542476+NicolasJoaquinParisi@users.noreply.github.com>
2023-11-22 21:05:55 -0300

    added update and delete operations for invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,7 +13,9 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Types validators
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   // Validate and prepare the data and extract the data from formData
@@ -27,7 +29,7 @@ export async function createInvoice(formData: FormData) {
 
   const date = new Date().toISOString().split('T')[0];
 
-  // Inserting the data into your database
+  // Inserting the data in the database
   await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
@@ -36,4 +38,31 @@ export async function createInvoice(formData: FormData) {
   // Revalidate and redirect
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  // Validate data
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  // Update data in the database
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  // Revalidate and redirect
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
